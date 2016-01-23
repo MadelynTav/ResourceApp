d8f016832137123d52d981925849ef9caa2bb511
madelyntav <madelyntav@gmail.com>
2016-01-23 13:41:19 -0500

    initial commit

---
+++ app/src/main/java/boston/hackingforgood/resourceapp/MainActivity.java
@@ -0,0 +1,13 @@
+package boston.hackingforgood.resourceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
