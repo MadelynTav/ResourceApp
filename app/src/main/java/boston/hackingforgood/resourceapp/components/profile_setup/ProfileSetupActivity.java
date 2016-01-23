1b12d372bec2f47d2d99053498288df8e0b17286
David Zhu <hdavidzhu@gmail.com>
2016-01-23 15:08:56 -0500

    ADD: Template skeletons for profile setup.

---
+++ app/src/main/java/boston/hackingforgood/resourceapp/components/profile_setup/ProfileSetupActivity.java
@@ -0,0 +1,47 @@
+package boston.hackingforgood.resourceapp.components.profile_setup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import boston.hackingforgood.resourceapp.R;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class ProfileSetupActivity extends AppCompatActivity {
+
+    @Bind(R.id.image_profile)
+    ImageView profileImage;
+
+    @OnClick(R.id.button_change_photo)
+    public void onChangePhotoButtonClicked() {
+
+    }
+
+    @Bind(R.id.edit_text_real_name)
+    EditText realNameEditText;
+
+    @Bind(R.id.edit_text_display_name)
+    EditText displayNameEditText;
+
+    @Bind(R.id.edit_text_email)
+    EditText emailEditText;
+
+    @Bind(R.id.edit_text_mobile)
+    EditText mobileEditText;
+
+    @Bind(R.id.autocomplete_organization)
+    AutoCompleteTextView organizationAutocomplete;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_setup);
+        ButterKnife.bind(this);
+    }
+}
